90f4f94aa7b4b87faa80ff86e8f67c18b81eb882
V-I-K-I <anystrelchenko@gmail.com>
2022-11-20 20:09:41 +0300

    создания гит

---
+++ src/main/java/com/example/telegrampetbot/TelegramPetBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrampetbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramPetBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramPetBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telegrampetbot/TelegramPetBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegrampetbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramPetBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
